34e1c6121e09d8bae927a3e89b5aab3ec069efbd
Martin Pyka <martin.pyka@gmx.de>
2016-11-18 20:50:42 +0100

    added testing for regular year payments

--- financial_life/financing/test_financing.py
+++ financial_life/financing/test_financing.py
@@ -21,35 +21,43 @@ class Test_Create_Stop_Criteria(unittest.TestCase):
     def test_callable(self):
         t = lambda x: x < datetime(2016,11,18)
         foo = financing.create_stop_criteria(t)
-        foo = financing.create_stop_criteria(t)
-        self.assertTrue(foo(datetime(2016,10,14)))
-        self.assertTrue(foo(datetime(2016,11,14)))
-        self.assertFalse(foo(datetime(2016,11,19)))
+        self.assertFalse(foo(datetime(2016,10,14)))
+        self.assertFalse(foo(datetime(2016,11,14)))
+        self.assertTrue(foo(datetime(2016,11,19)))
 
 class TestRegular_Month_Payment(unittest.TestCase):
 
 
     def setUp(self):
-        self.regular =  {'interval': 'month',
+        self.regular =  {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'month',
                          'day' : 15,
                          'date_start': Bank_Date(2015, 3, 15),
                          'date_stop': Bank_Date(2015, 6, 15),
                          'payment': 3000,
-                         'name' : 'Test'
+                         'name' : 'Test',
+                         'fixed': True
                          }
-        self.infinite =  {'interval': 'month',
+        self.infinite = {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'month',
                          'day' : 15,
                          'date_start': Bank_Date(2015, 3, 15),
                          'date_stop': Bank_Date.max,
                          'payment': 3000,
-                         'name' : 'Test'
+                         'name' : 'Test',
+                         'fixed': True
                          }
-        self.lastcal =  {'interval': 'month',
+        self.lastcal =  {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'month',
                          'day' : 31,
                          'date_start': Bank_Date(2015, 1, 31),
                          'date_stop': Bank_Date(2015, 6, 15),
                          'payment': 3000,
-                         'name' : 'Test'
+                         'name' : 'Test',
+                         'fixed': True
                          }
     
     def test_begin_payment_next_month(self):
@@ -84,5 +92,60 @@ class TestRegular_Month_Payment(unittest.TestCase):
         self.assertEqual(payment['date'], datetime(2015,2,28))
          
         
-#if __name__ == '__main__':
-#    unittest.main()
+class TestRegular_Year_Payment(unittest.TestCase):
+
+
+    def setUp(self):
+        self.regular =  {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'yearly',
+                         'day' : 15,
+                         'date_start': Bank_Date(2015, 3, 15),
+                         'date_stop': Bank_Date(2018, 3, 14),
+                         'payment': 3000,
+                         'name' : 'Test',
+                         'fixed': True
+                         }
+        self.infinite = {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'yearly',
+                         'day' : 15,
+                         'date_start': Bank_Date(2015, 3, 15),
+                         'date_stop': Bank_Date.max,
+                         'payment': 3000,
+                         'name' : 'Test',
+                         'fixed': True
+                         }
+        self.lastcal =  {'from_acc': 'Dummy',
+                         'to_acc': 'Dummy',
+                         'interval': 'yearly',
+                         'day' : 31,
+                         'date_start': Bank_Date(2015, 1, 31),
+                         'date_stop': Bank_Date(2017, 6, 15),
+                         'payment': 3000,
+                         'name' : 'Test',
+                         'fixed': True
+                         }
+    
+    def test_begin_payment_same_year(self):
+        iterator = financing.iter_regular_year(self.regular, date_start = datetime(2015,3, 15))
+        payment = next(iterator)
+        self.assertEqual(payment['date'], datetime(2015,3,15))
+        
+    
+    def test_begin_payment_next_year(self):
+        iterator = financing.iter_regular_year(self.infinite, date_start = datetime(2015,3, 16))
+        payment = next(iterator)
+        self.assertEqual(payment['date'], datetime(2016, 3, 15))
+            
+    def test_proper_sequence(self):
+        iterator = financing.iter_regular_year(self.regular, date_start = datetime(2015,3, 20))
+        payment = next(iterator)
+        self.assertEqual(payment['date'], datetime(2016,3,15))
+        payment = next(iterator)
+        self.assertEqual(payment['date'], datetime(2017,3,15))
+        self.assertRaises(StopIteration, next, iterator)
+        
+if __name__ == '__main__':
+    unittest.main()        
+        
