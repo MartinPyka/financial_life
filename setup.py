6ab733a46b9a47a603565fd6e7e5b51767738ae3
MartinPyka <martin.pyka@gmx.de>
2016-12-08 20:46:15 +0100

    changed requirements for pandas and pandas

--- setup.py
+++ setup.py
@@ -38,9 +38,9 @@ skw = dict(
 if have_setuptools is True:
 	skw['install_requires'] = [
 		'Jinja2>=2.7.2,<3',
-		'matplotlib>=1.5.3,<2',
+		'matplotlib>=1.3.1,<2',
 		'numpy>=1.11.2,<2',
-		'pandas>=0.19.1,<1',
+		'pandas>=0.18.1,<1',
 		'tabulate>=0.7.5,<1',
 	]
 
