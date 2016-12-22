d6690ea0f6ed412fc9e7863124b2f03f311c374a
Martin Pyka <martin.pyka@gmx.de>
2016-12-22 22:35:02 +0100

    extended example for usage of meta-data and controller

--- financial_life/examples/meta_data.py
+++ financial_life/examples/meta_data.py
@@ -14,21 +14,50 @@ from matplotlib.pyplot import show
 from financial_life.financing import accounts as a
 from financial_life.reports import html
 
-def example1():
-    # create a private bank account and a loan
-    account = a.Bank_Account(amount = 1000, interest = 0.001, name = 'Main account', date="01.09.2016")
-    loan = a.Loan(amount = 100000, interest = 0.01, name = 'House Credit', date="01.09.2016")
 
+def controller_tax(s):
+    """ This is a controller function that calculates annual tax rates
+    s is the simulation object
+    """
+    # perform tax calculation always on 15th of February, just to
+    # reflect the fact that tax payments for the previous year are
+    # never made on the 1st of January, which has an impact on 
+    # interests as well
+    if ((s.current_date.month == 2) and 
+       (s.current_date.day == 15)):
+        print('Taxtime')
+    
+
+def example_meta_controller():
+    """ This example shows, how meta-information for payments and account data could
+    be used to calculate annual tax-return """
+    account = a.Bank_Account(amount = 1000, interest = 0.001, name = 'Main account', date="01.09.2016")
+    loan = a.Loan(amount = 100000, interest = 0.01, name = 'House Credit', date="01.09.2016",
+                  meta = {'tax': {
+                                  'outcome': 'yearly_interests' 
+                                  }
+                          }
+                  )
+    
     # add these accounts to the simulation
     simulation = a.Simulation(account, loan, date='01.09.2016')
 
-    # describe single or regular payments between accounts. note, that
-    # a string can be used for external accounts that you don't want to model.
-    # also note the lambda function for the payments to the loan.
-    simulation.add_regular('Income', account, 2000, interval = 'monthly', date_start="01.09.2016")
+    # our employee receives monthly 2000 netto, coming from 2500 brutto,
+    # 310 are subtracted directly from the loan, which is less than she
+    # needs to pay. 190 are paid for insurance
+    simulation.add_regular('Income', account, 2000, interval = 'monthly', date_start="01.09.2016", 
+                           meta={'type': 'income', 
+                                 'tax': {
+                                         'brutto': 2500, 
+                                         'tax_paid': 310,
+                                         'insurance': 190
+                                         }
+                                }
+                           )
     # you can also use lambda function to dynamically decide how much money
     # you would like to transfer
-    simulation.add_regular(account, loan, lambda: min(1500, -loan.account), interval = 'monthly', date_start="01.09.2016", meta={'tax': 500})
+    simulation.add_regular(account, loan, lambda: min(1500, -loan.account), interval = 'monthly', date_start="01.09.2016")
+    simulation.add_controller(controller_tax)
 
     # simulate for ten years
     simulation.simulate(delta = timedelta(days=365*10))
@@ -39,4 +68,4 @@ def example1():
     return simulation
 
 if __name__ == '__main__':
-    example1()
+    example_meta_controller()
